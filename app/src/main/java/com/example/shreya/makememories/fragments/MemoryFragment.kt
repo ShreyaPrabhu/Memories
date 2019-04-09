4a164b4373241773c71b725152bf24878f8bc741
ShreyaPrabhu <shreyaprabhu96@gmail.com>
2019-04-09 15:46:36 +0530

    Add delete option in memory fragment

--- app/src/main/java/com/example/shreya/makememories/fragments/MemoryFragment.kt
+++ app/src/main/java/com/example/shreya/makememories/fragments/MemoryFragment.kt
@@ -2,12 +2,11 @@ package com.example.shreya.makememories.fragments
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.example.shreya.makememories.R
 import com.example.shreya.makememories.databinding.FragmentMemoryBinding
 import com.example.shreya.makememories.room.MemoryEntity
@@ -19,6 +18,11 @@ class MemoryFragment : Fragment() {
     private var memoryEntity: MemoryEntity? = null
     private lateinit var memoryViewModel: MemoryViewModel
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true);
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
@@ -36,5 +40,22 @@ class MemoryFragment : Fragment() {
         binding.imageDescription.text = memoryEntity!!.imageDescription
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return if (id == R.id.delete) {
+            // Delete image here
+            val memoryViewModel: MemoryViewModel
+            memoryViewModel = ViewModelProviders.of(this).get(MemoryViewModel::class.java)
+            memoryViewModel.deleteMemoryById(memoryEntity!!.id.toLong())
+            view!!.findNavController().navigate(R.id.action_memoryFragment_to_mainFragment2)
+            true
+        } else super.onOptionsItemSelected(item)
+
     }
 }
